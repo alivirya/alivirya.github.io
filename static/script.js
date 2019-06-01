9b213a2dae073979a9fdd09dd1c78417329d058f
Audrey Livirya <aliv074@aucklanduni.ac.nz>
2019-06-01 15:08:13 +1200

    Added text description and changed fonts

--- static/script.js
+++ static/script.js
@@ -1,13 +1,26 @@
-let index = 0;
+let photoIndex = 0;
+let textIndex = 0;
 function phoebeSlideshow() {
     let image = document.getElementById("phoebe")
     images = ["media/phoebe1.jpg", "media/phoebe2.jpg", "media/phoebe3.jpeg", "media/phoebe4.jpg", "media/phoebe5.jpg", "media/phoebe6.jpg", "media/phoebe7.jpg", "media/phoebe8.jpg"]
 
-    index++;
-    if (index >= images.length) {
-        index = 0;
+    photoIndex++;
+    if (photoIndex >= images.length) {
+        photoIndex = 0;
     }
-    image.src = images[index];
+    image.src = images[photoIndex];
+}
+
+function textSlideshow() {
+    let textId = document.getElementById("description");
+    let text = ["I'm 22 years old", "I've lived in six countries", "I go rock climbing",
+     "I'm Indonesian", "I have a dog named Phoebe"]
+    textIndex++;
+    if (textIndex >= text.length) {
+        textIndex = 0;
+    }
+    textId.innerHTML = text[textIndex];
 }
 
 setInterval(phoebeSlideshow, 3000);
+setInterval(textSlideshow, 3000);
