a73a45b14e9521ef3a3b611a723dd44ab375a77e
Audrey <aliv074@aucklanduni.ac.nz>
2019-12-05 14:28:42 +1300

    Changed files

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 let photoIndex = 0;
 function phoebeSlideshow() {
     let image = document.getElementById("phoebe")
-    images = ["media/phoebe1.jpg", "media/phoebe2.jpg", "media/phoebe3.jpeg", "media/phoebe4.jpg", "media/phoebe5.jpg", "media/phoebe6.jpg", "media/phoebe7.jpg", "media/phoebe8.jpg"]
+    images = ["media/phoebe1.jpg", "media/phoebe2.jpg", "media/phoebe3.jpeg", "media/phoebe4.jpg", "media/phoebe5.jpg", "media/phoebe6.jpg", "media/phoebe7.jpg"]
 
     photoIndex++;
     if (photoIndex >= images.length) {
