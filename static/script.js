0988c5979d55816476a8669bdf94d44366ae75c4
Audrey <aliv074@aucklanduni.ac.nz>
2019-12-05 13:15:54 +1300

    Made website more simple

---
+++ static/script.js
@@ -0,0 +1,13 @@
+let photoIndex = 0;
+function phoebeSlideshow() {
+    let image = document.getElementById("phoebe")
+    images = ["media/phoebe1.jpg", "media/phoebe2.jpg", "media/phoebe3.jpeg", "media/phoebe4.jpg", "media/phoebe5.jpg", "media/phoebe6.jpg", "media/phoebe7.jpg", "media/phoebe8.jpg"]
+
+    photoIndex++;
+    if (photoIndex >= images.length) {
+        photoIndex = 0;
+    }
+    image.src = images[photoIndex];
+}
+
+setInterval(phoebeSlideshow, 4000);
